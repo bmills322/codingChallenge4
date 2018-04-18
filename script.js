5193da35b88b695b4f42b3cb03b9e01b9f6b1eae
bmills322 <36277009+bmills322@users.noreply.github.com>
2018-04-17 22:54:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+fs.readFile('./santa.txt', (err, data) =>{
+	console.time('funchallenge')
+	if(!err) {
+		let direction = Array.from(data.toString());
+		let santaLocation = 0;
+
+		direction.map(currentFloor => {
+			if (currentFloor === "(") {
+				santaLocation++;
+			}
+			if (currentFloor === ")") {
+				santaLocation--;
+			};
+		});
+		console.log(santaLocation);
+	};
+	console.timeEnd('funchallenge')
+});
+
+//PART 2
